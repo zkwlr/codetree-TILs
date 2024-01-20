4ec1be5e26168d50d3f4f8723c9e98e3ade7a9af
zkwlr <bluefrog2k@yonsei.ac.kr>
2024-01-20 22:33:34 +0900

    add link

--- 240120/줄 세우기 2/line-up-students-2.cpp
+++ 240120/줄 세우기 2/line-up-students-2.cpp
@@ -1,16 +1,19 @@
+// https://blockdmask.tistory.com/178 :c++ : sort() 함수 사용법
 #include <iostream>
 using namespace std;
 
 #define MAX_SIZE 1000
 
-struct Student {
+struct Student
+{
     int height;
     int weight;
     int num;
 };
 
-void studentSort(struct Student arr[], int n) {
-    for (int i = n-1; i > 0; i--) // 원소 n개 정렬이면 버블정렬에선 n-1번 교환 시행
+void studentSort(struct Student arr[], int n)
+{
+    for (int i = n - 1; i > 0; i--) // 원소 n개 정렬이면 버블정렬에선 n-1번 교환 시행
     {
         for (int j = 0; j < i; j++)
         {
@@ -20,10 +23,12 @@ void studentSort(struct Student arr[], int n) {
                 arr[j + 1] = arr[j];
                 arr[j] = temp;
             }
-            else if ( arr[j].height == arr[j+1].height) { // 키가 동일하다면
-                if (arr[j].weight < arr[j + 1].weight) {
-                    struct Student temp = arr[j+1];
-                    arr[j+1] = arr[j];
+            else if (arr[j].height == arr[j + 1].height)
+            { // 키가 동일하다면
+                if (arr[j].weight < arr[j + 1].weight)
+                {
+                    struct Student temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
                     arr[j] = temp;
                 }
             }
@@ -31,22 +36,26 @@ void studentSort(struct Student arr[], int n) {
     }
 }
 
-int main() {
+int main()
+{
     // 여기에 코드를 작성해주세요.
-    int n; cin >> n;
+    int n;
+    cin >> n;
     struct Student arr[MAX_SIZE];
-    for (int i = 0; i < n; i++) { // 학생 정보 저장
+    for (int i = 0; i < n; i++)
+    { // 학생 정보 저장
         struct Student student;
         cin >> student.height >> student.weight;
-        student.num = i+1;
+        student.num = i + 1;
         arr[i] = student;
     }
 
     studentSort(arr, n);
-    for (int i = 0; i < n; i++) {
-        cout << arr[i].height << " " 
-        << arr[i].weight << " " 
-        << arr[i].num << " " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i].height << " "
+             << arr[i].weight << " "
+             << arr[i].num << " " << endl;
     }
     return 0;
 }
